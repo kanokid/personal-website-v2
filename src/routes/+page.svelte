3edc53fced077c947e5c29ac0f094c83b9912aa7
Colten Brant <57643139+kanokid@users.noreply.github.com>
2026-04-28 17:44:23 +0000

    started svelte site and transferred stuff over, still lots to go and bugs to fix

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<body>
+    <h1>Colten Brant </h1>
+    <p> I am 13 yrs old </p>
+    <p> I love playing with tech.</p>
+    <h3>Some of my favorite things to do are:</h3>
+    <ul>
+        <li>Have AI vibe code apps for me</li>
+        <li>3d print fun stuff on my A1 mini</li>
+        <li>Work on PCB design</li>
+        <li>Play/mess with linux and rice it</li>
+    </ul>
+    <p> My favorite linux distros are Fedora and Ubuntu (mostly fedora though).</p>
+    <img
+        src="https://cdn.hackclub.com/019dd520-aa8c-7b6a-b175-5427d688d398/img_0507.png"
+        alt="Fedora"
+    />
+    <img
+        src="https://cdn.hackclub.com/019dd520-aa8c-7b6a-b175-5427d688d398/img_0507.png"
+        alt="Ubuntu"
+    /> 
+    <h3>My favorite desktops for linux are:</h3>
+    <ul>
+        <li><strong>niri Tiling WM (with noctalia)</strong></li>
+        <li><strong>KDE Plasma</strong> (I like the similarities to windows and the extreme out of box customizability<br>and how easy it is to use, but sometimes I like experiencing others too.)</li>
+        <li><strong>GNOME</strong> (This one has really grown on me, but I like the way <a href="https://zorin.com/os/" style="color: inherit;">Zorin OS</a> did it and the look of the overview.)</li>
+        <li><strong>COSMIC</strong> (COSMIC is nice because it can both tile and float, while being easy to use and sometimes,<br>the unique style vibes with me.)</li>
+    </ul>
+    <p>
+<!--          <strong><a href="gnome.html" style="color: inherit;">Click here to see my GNOME setup!</a></strong> -->
+    </p>
+    <h3>Some of my goals for when I have time are:</h3>
+    <ul>
+        <li>Learn some interpreted language like Python, Ruby, or Lua.</li>
+<!--          <li>Try to sell some of my designs on Tindie.(maybe if I have a good idea, but probably not.)</li> -->
+    </ul>
+    
+    <h3>Some of my current/finished projects are:</h3>
+    <ul>
+        <li>
+            <strong>Electromagnetic Levitator</strong> (built, but could't figure out what was wrong with my design, so I gave up)
+            <p>(It was supposed to make a strong N52 magnet float<br>using a custom active attractive electromagnet system)</p>
+        </li>
+        <li>
+            <strong>WLEDnano (WIP)</strong>
+            <p>This tiny little board can run WLED using the ESP32-S3-MINI-1 chipset<br>and control up to 2 sets addressable 5V LEDs (*it technically has 2 data pins, but you would probably run out of power with 2,<br>but there might not be enough to run 1 strip (depending on strip length)) while being the size of your thumb<br>and being able to power it with the included USB-C port. (hopefully, but probably not enough power to run bigger strips (ex. matrixes))</p>
+        </li>
+        <li>
+            <strong>D1 mini WLED shield</strong>
+            <p>This was the project that i spent weeks working and waiting, and then when I built it and plugged it in, it blew a tantalum capacitor and blew burn holes into my rug.<br>
+            The specs it was supposed to have was a ESP32 D1 mini running WLED, on a custom shield that had a LM2596-5.0 buck converter circuitry<br>(probably where I went wrong, because they are complex, and I had a ton of bugs in my design),<br>A fuse (put on the 5V line by accident, not the 12V line in), A level shifter for data stability,<br>and a spot to plug in a MAX9814 module with a gain jumper for audioreactive WLED,<br>and 2 terminal blocks for hookup (turns out I used the wrong footprint, so they didn't fit).</p>
+        </li>
+        <li>
+            <strong>Memory Game</strong>
+            <p>This program is wroten in python with the cmu_graphics library, a python library I was taught in school.<br>It has the python interpreter and the cmu_graphics package included, so you can just click the batch file and play.<br>I built it for the Hack Club YSWS fudge fudge fudge, and the symbols are all holiday themed.<br>added victory sound effects and have it so the patterns are random each time.</p>
+        </li>
+    </ul>
+    <p>
+    <strong><a href="https://github.com/kanokid/memory-game" style="color: inherit;">Click here to download the windows version of the memory game!</a></strong>
+    <br>
+    (It will work on Mac or Linux if you have Python 3.12 and the cmu_graphics library installed.)
+    </p>
+    <ul>
+        <li>
+            <strong>Various games I've made on <a href="https://kanokid.itch.io" style="color: inherit;">itch.io</a></strong>
+            <p><a href="https://kanokid.itch.io" style="color: inherit;">Click here to look at all the games I have made on itch.io!</a></p>
+        </li>
+        </ul>
+    <p>
+        <br>
+        <small><a href="https://github.com/kanokid/bug-free-succotash-website" style="color: inherit;">oh, and if you want, here is the code for my website.</a></small>
+    </p>
+</body>
